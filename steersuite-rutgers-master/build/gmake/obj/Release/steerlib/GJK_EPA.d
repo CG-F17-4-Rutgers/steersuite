6d07b4a0ced6378bbb1a917e71864b341c1be943
Taichi Aritomo <taichiaritomo@gmail.com>
2017-09-22 13:14:42 -0400

    add steersuite build

---
+++ steersuite-rutgers-master/build/gmake/obj/Release/steerlib/GJK_EPA.d
@@ -0,0 +1,10 @@
+obj/Release/steerlib/GJK_EPA.o: ../../steerlib/src/GJK_EPA.cpp \
+  ../../steerlib/include/obstacles/GJK_EPA.h \
+  ../../steerlib/include/util/Geometry.h \
+  ../../steerlib/include/Globals.h
+
+../../steerlib/include/obstacles/GJK_EPA.h:
+
+../../steerlib/include/util/Geometry.h:
+
+../../steerlib/include/Globals.h:
