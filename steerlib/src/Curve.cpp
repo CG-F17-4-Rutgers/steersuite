362130ac64f670f46bf55dabb2ece5e6b4a33232
Taichi Aritomo <taichiaritomo@gmail.com>
2017-09-23 18:58:05 -0400

    implemented useHermiteCurve and drawCurve
    need to check if what we're seeing in provided testcases is correct, since it doesn't seem dramatically different from straight lines. however, with custom test case curve_taichi.xml that uses crazy tangents, it looks more like what you'd expect from curves.

--- steerlib/src/Curve.cpp
+++ steerlib/src/Curve.cpp
@@ -6,6 +6,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <math.h>
 #include <util/Geometry.h>
 #include <util/Curve.h>
 #include <util/Color.h>
@@ -45,19 +46,27 @@ void Curve::drawCurve(Color curveColor, float curveThickness, int window)
 {
 #ifdef ENABLE_GUI
 
-	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
-	if (!flag)
-	{
-		std::cerr << "ERROR>>>>Member function drawCurve is not implemented!" << std::endl;
-		flag = true;
-	}
-	//=========================================================================
-
 	// Robustness: make sure there is at least two control point: start and end points
 	// Move on the curve from t=0 to t=finalPoint, using window as step size, and linearly interpolate the curve points
 	// Note that you must draw the whole curve at each frame, that means connecting line segments between each two points on the curve
+	if (!checkRobust())
+		return;
+
+	float t_i = controlPoints[0].time + (float)window;
+	const float t_final = controlPoints[controlPoints.size() - 1].time;
+
+	Point p_i_minus_1 = controlPoints[0].position;
+	Point p_i;
+
+	while (t_i <= t_final)
+	{
+		calculatePoint(p_i, t_i);
+		DrawLib::drawLine(p_i_minus_1, p_i, curveColor, curveThickness);
+		p_i_minus_1 = p_i;
+		t_i = t_i + window;
+	}
 	
+
 	return;
 #endif
 }
@@ -73,21 +82,24 @@ void Curve::sortControlPoints()
 {
 	
 	// FOR TESTING -- before sort
-	/*
-	for (CurvePoint pt : controlPoints)
+	
+	/*for (CurvePoint pt : controlPoints)
 		std::cout << pt.time << " ";
-	std::cout << std::endl;
-	*/
+	std::cout << std::endl;*/
+	
 
 	// Sort vector of control points
 	std::sort(controlPoints.begin(), controlPoints.end(), compareControlPoints);
 
 	// FOR TESTING -- after sort
-	/*
-	for (CurvePoint pt : controlPoints)
-	std::cout << pt.time << " ";
-	std::cout << std::endl;
-	*/
+	/*for (CurvePoint pt : controlPoints)
+	{
+		std::cout << pt.time << " ";
+		std::cout << pt.position << " ";
+		std::cout << pt.tangent << " ";
+		std::cout << std::endl;
+	}*/
+	
 }
 
 // Calculate the position on curve corresponding to the given time, outputPoint is the resulting position
@@ -124,24 +136,25 @@ bool Curve::calculatePoint(Point& outputPoint, float time)
 bool Curve::checkRobust()
 {
 	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
+	/*static bool flag = false;
 	if (!flag)
 	{
 		std::cerr << "ERROR>>>>Member function checkRobust is not implemented!" << std::endl;
 		flag = true;
-	}
+	}*/
 	//=========================================================================
 
+	return controlPoints.size() >= 2;
 
-	return true;
+	// return true;
 }
 
 // Find the current time interval (i.e. index of the next control point to follow according to current time)
 bool Curve::findTimeInterval(unsigned int& nextPoint, float time)
 {
-
 	// assign nextPoint the index of the next control point. return false if there is no next point
-	// binary search: terminate if exact time is found. otherwise, let it rock and return the mid. 
+	// using binary search: terminate if exact time is found. otherwise, let it rock and the resulting lo will be the index of the next point. 
+	// std::cout << "Looking for next point after time " << time << std::endl;
 	unsigned int lo = 0, mid, hi = controlPoints.size() - 1;
 	while (lo <= hi)
 	{
@@ -152,14 +165,23 @@ bool Curve::findTimeInterval(unsigned int& nextPoint, float time)
 			hi = mid - 1;
 		else
 		{
-			nextPoint = (unsigned int) mid + 1;
+			nextPoint = mid + 1; // found point at exact time, so return next point
+			// std::cout << "BS Next point found. Its index is: " << nextPoint << std::endl;
 			return true;
 		}
 	}
-	if (mid >= controlPoints.size())
-		return false;
+
+	if (lo >= controlPoints.size())
+	{
+		// std::cout << "No next point found." << std::endl;
+		return false; // no next point
+	}
 	else
-		nextPoint = (unsigned int) mid;
+	{
+		nextPoint = lo;
+		// std::cout << "Next point found. Its index is: " << nextPoint << std::endl;
+	}
+
 	return true;
 }
 
@@ -170,15 +192,32 @@ Point Curve::useHermiteCurve(const unsigned int nextPoint, const float time)
 	float normalTime, intervalTime;
 
 	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
+	/*static bool flag = false;
 	if (!flag)
 	{
 		std::cerr << "ERROR>>>>Member function useHermiteCurve is not implemented!" << std::endl;
 		flag = true;
-	}
+	}*/
 	//=========================================================================
 
+	const unsigned int currentPoint = nextPoint - 1;
+	assert(currentPoint >= 0 && currentPoint < controlPoints.size());
+	intervalTime = controlPoints[nextPoint].time - controlPoints[currentPoint].time;
+	normalTime = (time - controlPoints[currentPoint].time) / intervalTime;
+	// std::cout << time << " " << currentPoint << " " << controlPoints[currentPoint].time << std::endl;
+	// std::cout << "normalized time: " << normalTime << std::endl;
+
 	// Calculate position at t = time on Hermite curve
+	// assert(time >= 0 && time <= 1); // the following depends on this to be true. if it's not, set const float t = (time - controlPoints[currentPoint].time) / (controlPoints[nextPoint].time - controlPoints[currentPoint].time);
+	const float t = normalTime;
+	const float t3 = std::pow(t, 3);
+	const float t2 = std::pow(t, 2);
+
+	// Using hermite curve formula
+	newPosition = (2 * t3 - 3 * t2 + 1) * controlPoints[currentPoint].position
+		+ (-2 * t3 + 3 * t2) * controlPoints[nextPoint].position
+		+ (t3 - 2 * t2 + t) * controlPoints[currentPoint].tangent
+		+ (t3 - t2) * controlPoints[nextPoint].tangent;
 
 	// Return result
 	return newPosition;
